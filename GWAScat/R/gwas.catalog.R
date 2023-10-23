87e6dbfac7e39f7dd1c6c7ace636cd4d2e22d150
Gero1999 <gerardojoro@gmail.com>
2023-10-23 15:20:50 +0200

    add GWAScat package

---
+++ GWAScat/R/gwas.catalog.R
@@ -0,0 +1,10 @@
+#' Download metadata of the current GWAS catalog information
+#'
+#' @description
+#' Dataset with metadata from GWAS-Catalog summary statitistics from the October 2023
+#'
+#'
+#' @examples
+#'
+#'@export
+gwas.catalog = read.csv(system.file('data', 'gwcat.csv', package='GWAScat'))
